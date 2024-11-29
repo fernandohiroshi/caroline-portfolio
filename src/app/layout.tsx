bbb205bef96681f9ac7bc6a10a72b9c64af289a7
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2024-11-29 05:55:36 -0300

    feat: update and configure Home and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next'
-import { Montserrat_Alternates } from 'next/font/google'
 import './globals.css'
+import { Montserrat_Alternates } from 'next/font/google'
+import Link from 'next/link'
+import { PiArrowCircleUpFill } from 'react-icons/pi'
+
+import { Footer } from '@/components/view/footer/Footer'
+import { Header } from '@/components/view/header/Header'
 
 const m = Montserrat_Alternates({
   subsets: ['latin'],
@@ -18,8 +23,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={`${m.className} antialiased`}>{children}</body>
+    <html lang="pt-BR" className="!scroll-smooth">
+      <body className={`${m.className} antialiased`}>
+        <Header />
+        {children}
+        <Footer />
+        <Link href="#" className="fixed bottom-3 right-3">
+          <PiArrowCircleUpFill
+            size={30}
+            className="opacity-60 duration-300 ease-in-out hover:scale-110 hover:opacity-100"
+          />
+        </Link>
+      </body>
     </html>
   )
 }
