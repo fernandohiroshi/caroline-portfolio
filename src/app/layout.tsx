9678ef481d2a2e13a43317e15d25be3870fd77d5
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2024-11-29 06:25:53 -0300

    feat: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,55 @@ const m = Montserrat_Alternates({
 
 export const metadata: Metadata = {
   title: 'Caroline Souza Design',
-  description: 'Portfolio',
+  description:
+    'Portfolio de Caroline Souza, especializada em design gráfico, fotografia profissional, UX/UI e muito mais.',
+  keywords: [
+    'Design gráfico',
+    'Fotografia profissional',
+    'UX/UI design',
+    'Interface do usuário',
+    'Experiência do usuário',
+    'Identidade visual',
+    'Branding',
+    'Tipografia',
+    'Ilustração digital',
+    'Portfólio criativo',
+    'Direção de arte',
+    'Marketing digital',
+    'Redes sociais',
+    'Gestão de conteúdo',
+    'Estratégias visuais',
+    'Publicidade online',
+    'Edição de imagens',
+    'Criação de logotipos',
+    'Layout responsivo',
+    'Estratégias de engajamento',
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+    },
+  },
+  openGraph: {
+    title: 'Caroline Souza Design - Portfolio',
+    description:
+      'Explore o portfólio de Caroline Souza, uma profissional em design gráfico, UX/UI, fotografia e branding, com um olhar criativo e estratégico.',
+    url: 'https://carolinesouzadesign.art',
+    siteName: 'Caroline Souza Design',
+    images: [
+      {
+        url: 'https://imgix.cosmicjs.com/a2cf1460-ae33-11ef-8a63-eb57d6c77a36-carolinesouzasite.png',
+        width: 1200,
+        height: 630,
+        alt: 'Visão geral do portfólio de Caroline Souza Design',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
